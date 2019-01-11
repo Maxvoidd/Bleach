4ab5cd5b91cf65b5f135b1ea8a440b4ea26ad9a8
Max <max.foti93@hotmail.it>
2019-01-11 20:34:26 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function add (x, y) {
+    var result = x + y;
+    return result;
+}
+
+var foo = add (2 , 3);
+console.log (foo);
+
+function div (x, y) {
+    var result = x / y;
+    return result;
+}
+
+var bar = div (6, 4);
+console.log ( bar );
